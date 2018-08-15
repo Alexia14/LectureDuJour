022494ba34fb01f4a054b757de38a5434f8ecc71
AlexiaC <alex.couton@gmail.com>
2018-08-15 11:09:25 +0200

    First pages

---
+++ nav.php
@@ -0,0 +1,27 @@
+<nav id="nav">
+	<ul>
+		<li><a href="index.php">Accueil</a></li>
+		<li>
+			<a href="#">Roman</a>
+			<ul>
+				<li><a href="#">Policer</a></li>
+				<li><a href="#">Magna phasellus</a></li>
+				<li><a href="#">Etiam dolore nisl</a></li>
+				<li>
+					<a href="#">And a submenu &hellip;</a>
+					<ul>
+						<li><a href="#">Lorem ipsum dolor</a></li>
+						<li><a href="#">Phasellus consequat</a></li>
+						<li><a href="#">Magna phasellus</a></li>
+						<li><a href="#">Etiam dolore nisl</a></li>
+					</ul>
+				</li>
+				<li><a href="#">Veroeros feugiat</a></li>
+			</ul>
+		</li>
+		<li><a href="left-sidebar.php">Théâtre</a></li>
+		<li><a href="right-sidebar.php">BD</a></li>
+		<li><a href="no-sidebar.php">A propos</a></li>
+		<li>Contact</li>
+	</ul>
+</nav>
