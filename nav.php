15619b0bab6e333d1f517d3c9fc9b1b919821b80
AlexiaC <alex.couton@gmail.com>
2018-09-28 22:32:13 +0200

    organization + links

--- nav.php
+++ nav.php
@@ -1,44 +1,43 @@
 <nav id="nav" class="cl-effect-1">
 	<ul>
-		<li><a href="index.php">Accueil</a></li>
+		<li><a href="/LectureDuJour/index.php">Accueil</a></li>
 		<li>
-			<a href="search_reading.php">Oeuvres</a>
+			<a href="/LectureDuJour/pages/search_reading.php">Oeuvres</a>
 			<ul>
 				<li>
-					<a href="novel.php">Roman</a>
+					<a href="/LectureDuJour/pages/novel.php">Roman</a>
 					<ul>
-						<li><a href="autobiography.php">Autobiographique</a></li>
-						<li><a href="literature.php">Littéraire</a></li>
-						<li><a href="detective.php">Policier</a></li>
+						<li><a href="/LectureDuJour/pages/novel/autobiography.php">Autobiographique</a></li>
+						<li><a href="/LectureDuJour/pages/novel/literature.php">Littéraire</a></li>
+						<li><a href="/LectureDuJour/pages/novel/detective.php">Policier</a></li>
 					</ul>
 				</li>
 				<li>
-					<a href="theater.php">Théâtre</a>
+					<a href="/LectureDuJour/pages/theater.php">Théâtre</a>
 					<ul>
-						<li><a href="absurd.php">Absurde</a></li>
-						<li><a href="comedy.php">Comédie</a></li>
+						<li><a href="/LectureDuJour/pages/theater/absurd.php">Absurde</a></li>
+						<li><a href="/LectureDuJour/pages/theater/comedy.php">Comédie</a></li>
 						</li>
 					</ul>
 				</li>
 				<li>
-					<a href="youth.php">Jeunesse</a>
+					<a href="/LectureDuJour/pages/youth.php">Jeunesse</a>
 					<ul>
-						<li><a href="fiction.php">Fiction</a></li>
-						</li>
+						<li><a href="/LectureDuJour/pages/youth/fiction.php">Fiction</a></li>
 					</ul>
 				</li>
 				<li>
-					<a href="comic.php">BD</a>
+					<a href="/LectureDuJour/pages/comic.php">BD</a>
 					<ul>
-						<li><a href="fantastic.php">Fantastique</a></li>
-						<li><a href="cartoon.php">Humoristique</a></li>
+						<li><a href="/LectureDuJour/pages/comic/fantastic.php">Fantastique</a></li>
+						<li><a href="/LectureDuJour/pages/comic/cartoon.php">Humoristique</a></li>
 						</li>
 					</ul>
 				</li>
 			</ul>
 		</li>
-		<li><a href="search_reading.php">Rechercher</a></li>
-		<li><a href="new_reading.php">Nouvelle saisie</a></li>
-		<li><a href="connect.php">Connexion</a></li>
+		<li><a href="/LectureDuJour/pages/search_reading.php">Rechercher</a></li>
+		<li><a href="/LectureDuJour/pages/new_reading.php">Nouvelle saisie</a></li>
+		<li><a href="/LectureDuJour/pages/connect.php">Connexion</a></li>
 	</ul>
 </nav>
