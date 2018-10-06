9da0154831dce209b1656d17ee6c96249a7f4b43
AlexiaC <alex.couton@gmail.com>
2018-10-06 22:22:14 +0200

    form in search page

--- pages/search.php
+++ pages/search.php
@@ -33,7 +33,111 @@
 
         <div class="container">
             <article id="main" class="special">
+                <header>
+                    <h2 style="color: #df7366; margin-bottom: 60px;">Vous cherchez une lecture particulière ?</h2>
+                </header>
 
+                <form method="post">
+                    <table class="formulaire">
+                        <tr>
+                            <td class="form_col1">Titre :</td>
+                            <td colspan="3"><input type="text" name="titre" placeholder="Titre"/></td>
+                        </tr>
+                        <tr>
+                            <td class="form_col1">Auteur :</td>
+                            <td colspan="3"><input type="text" name="auteur" placeholder="Auteur"/></td>
+                        </tr>
+                        <tr>
+                            <td class="form_col1">Style :</td>
+                            <td colspan="3">
+                                <select name="genre" onchange="style_livre(this.value)">
+                                    <option>-- sélectionner --</option>
+                                    <option value="Roman">Roman</option>
+                                    <option value="Nouvelle">Nouvelle</option>
+                                    <option value="Théâtre">Théâtre</option>
+                                    <option value="Poésie">Poésie</option>
+                                    <option value="Jeunesse">Jeunesse</option>
+                                    <option value="BD">BD</option>
+                                    <option value="Autre">Autre</option>
+                                </select>
+
+                                <div id="blockRoman" class="block_style_livre">
+                                    <input type="radio" name="style" value="Autobiographique">Autobiographie
+                                    <input type="radio" name="style" value="Littéraire">Littéraire
+                                    <input type="radio" name="style" value="Policier">Policier
+                                    <div style="display: flex;">
+                                        <input type="radio" name="style" style="float: left; margin-top: auto; margin-bottom: auto;"><input type="text" name="newStyle" placeholder="autre" style="width: 90%;">
+                                    </div>
+                                </div>
+                                <div id="blockNouvelle" class="block_style_livre">
+                                    <div style="display: flex;">
+                                        <input type="radio" name="style" style="float: left; margin-top: auto; margin-bottom: auto;"><input type="text" name="newStyle" placeholder="autre" style="width: 90%;">
+                                    </div>
+                                </div>
+                                <div id="blockThéâtre" class="block_style_livre">
+                                    <input type="radio" name="style" value="Absurde">Absurde
+                                    <input type="radio" name="style" value="Comédie">Comédie
+                                    <div style="display: flex;">
+                                        <input type="radio" name="style" style="float: left; margin-top: auto; margin-bottom: auto;"><input type="text" name="newStyle" placeholder="autre" style="width: 90%;">
+                                    </div>
+                                </div>
+                                <div id="blockPoésie" class="block_style_livre">
+                                    <div style="display: flex;">
+                                        <input type="radio" name="style" style="float: left; margin-top: auto; margin-bottom: auto;"><input type="text" name="newStyle" placeholder="autre" style="width: 90%;">
+                                    </div>
+                                </div>
+                                <div id="blockJeunesse" class="block_style_livre">
+                                    <input type="radio" name="style" value="Fiction">Fiction
+                                    <div style="display: flex;">
+                                        <input type="radio" name="style" style="float: left; margin-top: auto; margin-bottom: auto;"><input type="text" name="newStyle" placeholder="autre" style="width: 90%;">
+                                    </div>
+                                </div>
+                                <div id="blockBD" class="block_style_livre">
+                                    <input type="radio" name="style" value="Fantastique">Fantastique
+                                    <input type="radio" name="style" value="Humoristique">Humoristique
+                                    <div style="display: flex;">
+                                        <input type="radio" name="style" style="float: left; margin-top: auto; margin-bottom: auto;"><input type="text" name="newStyle" placeholder="autre" style="width: 90%;">
+                                    </div>
+                                </div>
+                                <div id="blockAutre" class="block_style_livre">
+                                    <div style="display: flex;">
+                                        <input type="text" name="newGenre" placeholder="genre ex: roman" style="width: 40%; margin-right: 5%;">
+                                        <input type="text" name="newStyle" placeholder="style ex: autobiographie" style="width: 40%;">
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="form_col1">Nationalité :</td>
+                            <td colspan="3"><input type="text" name="nationalite" placeholder="Nationalité"/></td>
+                        </tr>
+                        <tr>
+                            <td class="form_col1">Date de parution :</td>
+                            <td style="width: 20%;">
+                                <select name="mois">
+                                    <option>-- mois --</option>
+                                    <option value="Janvier">Janvier</option>
+                                    <option value="Février">Février</option>
+                                    <option value="Mars">Mars</option>
+                                    <option value="Avril">Avril</option>
+                                    <option value="Mai">Mai</option>
+                                    <option value="Juin">Juin</option>
+                                    <option value="Juillet">Juillet</option>
+                                    <option value="Aôut">Aôut</option>
+                                    <option value="Septembre">Septembre</option>
+                                    <option value="Octobre">Octobre</option>
+                                    <option value="Novembre">Novembre</option>
+                                    <option value="Décembre">Décembre</option>
+                                </select>
+                            </td>
+                            <td style="width: 1%;"></td>
+                            <td><input type="number" name="annee" min="1" max="2020" placeholder="année (aaaa)"/></td>
+                        </tr>
+                        <tr>
+                            <td colspan="3"><input type="submit" value="Valider"/></td>
+                        </tr>
+                    </table>
+                </form>
             </article>
             <hr />
             <div class="row">
