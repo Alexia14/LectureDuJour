db7ea693ef1be1a40ce474e5c09bd0223c6bc0a6
AlexiaC <alex.couton@gmail.com>
2018-10-10 23:47:46 +0200

    footer

--- footer.php
+++ footer.php
@@ -4,93 +4,91 @@
 
 <div id="footer">
     <div class="container">
-        <!--
-		<div class="row">
-        -->
-        <!-- Tweets -->
-        <!--
-            <section class="col-4 col-12-mobile">
+        <div class="row">
+            <!--
+            <section class="contact">
+                <header>
+                    <h3>Retrouvez-moi ici</h3>
+                </header>
+                <ul class="icons">
+                    <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+                    <li><a href="#" class="icon fa-linkedin"><span class="label">Linkedin</span></a></li>
+                </ul>
+            </section>
+            -->
+            <section class="col-3 col-12-mobile">
                 <header>
                     <h2 class="icon fa-address-card circled"><span class="label">Contact</span></h2>
                 </header>
                 <ul class="divided">
                     <li>
-                        <article class="tweet">
-                            Amet nullam fringilla nibh nulla convallis tique ante sociis accumsan.
-                            <span class="timestamp">5 minutes ago</span>
-                        </article>
+                        <div class="row">
+                            <span class="col-3 fa fa-map-marker" style="text-align: center; font-size: xx-large;"></span>
+                            <p class="col-9">ESME Sudria,<br />38 Rue Molière,<br />94205 Ivry-sur-Seine</p>
+                        </div>
+                        <div class="row">
+                            <span class="col-3 fa fa-at" style="text-align: center; font-size: xx-large"></span>
+                            <p class="col-9">alex.couton@gmail.com</p>
+                        </div>
                     </li>
                     <li>
-                        <article class="tweet">
-                            Hendrerit rutrum quisque.
-                            <span class="timestamp">30 minutes ago</span>
-                        </article>
-                    </li>
-                    <li>
-                        <article class="tweet">
-                            Curabitur donec nulla massa laoreet nibh. Lorem praesent montes.
-                            <span class="timestamp">3 hours ago</span>
-                        </article>
-                    </li>
-                    <li>
-                        <article class="tweet">
-                            Lacus natoque cras rhoncus curae dignissim ultricies. Convallis orci aliquet.
-                            <span class="timestamp">5 hours ago</span>
-                        </article>
+                        <section class="contact">
+                            <ul class="icons">
+                                <li style="border-top: 0; margin: 0; padding: 0;">
+                                    <a href="https://github.com/Alexia14" class="icon fa-github">
+                                        <span class="label">Github</span>
+                                    </a>
+                                </li>
+                                <li style="border-top: 0; margin: 0; padding: 0;">
+                                    <a href="https://www.facebook.com/gwannaelle.verfaillie" class="icon fa-facebook">
+                                        <span class="label">Facebook</span>
+                                    </a>
+                                </li>
+                                <li style="border-top: 0; margin: 0; padding: 0;">
+                                    <a href="https://www.linkedin.com/in/alexia-couton-0b0a84154/" class="icon fa-linkedin">
+                                        <span class="label">Linkedin</span>
+                                    </a>
+                                </li>
+                                <li style="border-top: 0; margin: 0; padding: 0;">
+                                    <a class="icon fa-at iconFooterMail" title="alex.couton@gmail.com">
+                                        <span class="label">alex.couton@gmail.com</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </section>
                     </li>
                 </ul>
             </section>
-        -->
-        <!-- Posts -->
-        <!--
-            <section class="col-4 col-12-mobile">
+
+            <section class="col-3 col-12-mobile">
                 <header>
-                    <h2 class="icon fa-file circled"><span class="label">Posts</span></h2>
+                    <h2 class="icon fa-file circled"><span class="label">Liens</span></h2>
                 </header>
                 <ul class="divided">
                     <li>
-                        <article class="post stub">
-                            <header>
-                                <h3><a href="#">Nisl fermentum integer</a></h3>
-                            </header>
-                            <span class="timestamp">3 hours ago</span>
-                        </article>
+                        <p style="text-align: center;">
+                            <a style="border: none;" href="http://evene.lefigaro.fr/citations">Citations</a>
+                        </p>
+                        <p style="text-align: center;">
+                            <a style="border: none;" href="https://pixabay.com/fr/photos/?q=lecture&hp=&image_type=all&order=&cat=&min_width=&min_height=">Images</a>
+                        </p>
+                        <p style="text-align: center;">
+                            <a style="border: none;" href="https://www.assistancescolaire.com/">Définitions</a>
+                        </p>
                     </li>
                     <li>
-                        <article class="post stub">
-                            <header>
-                                <h3><a href="#">Phasellus portitor lorem</a></h3>
-                            </header>
-                            <span class="timestamp">6 hours ago</span>
-                        </article>
-                    </li>
-                    <li>
-                        <article class="post stub">
-                            <header>
-                                <h3><a href="#">Magna tempus consequat</a></h3>
-                            </header>
-                            <span class="timestamp">Yesterday</span>
-                        </article>
-                    </li>
-                    <li>
-                        <article class="post stub">
-                            <header>
-                                <h3><a href="#">Feugiat lorem ipsum</a></h3>
-                            </header>
-                            <span class="timestamp">2 days ago</span>
-                        </article>
+                        <p style="text-align: center;"><a href="/LectureDuJour/contact.php" style="border: none">Qui sommes-nous ?</a></p>
+                        <p style="text-align: center;"><a href="/LectureDuJour/how_choose.php" style="border: none">Comment choisir une oeuvre ?</a></p>
                     </li>
                 </ul>
             </section>
-        -->
-        <!-- Photos -->
-        <!--
-            <section class="col-4 col-12-mobile">
+
+            <section class="col-6 col-12-mobile">
                 <header>
                     <h2 class="icon fa-book circled"><span class="label">Oeuvres</span></h2>
                 </header>
                 <div class="row gtr-25">
-                    <div class="col-6">
+                    <div class="col-4">
                         <a href="/LectureDuJour/pages/novel.php" class="image fit">
                             <img src="/LectureDuJour/images/roman.jpg" alt="Roman" />
                             <div class="overlay">
@@ -98,7 +96,15 @@
                             </div>
                         </a>
                     </div>
-                    <div class="col-6">
+                    <div class="col-4">
+                        <a href="/LectureDuJour/pages/novella.php" class="image fit">
+                            <img src="/LectureDuJour/images/nouvelle.jpg" alt="Nouvelle" />
+                            <div class="overlay">
+                                <div class="overlay_text">Nouvelle</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-4">
                         <a href="/LectureDuJour/pages/theater.php" class="image fit">
                             <img src="/LectureDuJour/images/théatre.jpg" alt="Théâtre" />
                             <div class="overlay">
@@ -106,7 +112,15 @@
                             </div>
                         </a>
                     </div>
-                    <div class="col-6">
+                    <div class="col-4">
+                        <a href="/LectureDuJour/pages/poetry.php" class="image fit">
+                            <img src="/LectureDuJour/images/poesie.jpg" alt="Poésie" />
+                            <div class="overlay">
+                                <div class="overlay_text">Poésie</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-4">
                         <a href="/LectureDuJour/pages/youth.php" class="image fit">
                             <img src="/LectureDuJour/images/jeunesse.jpg" alt="Jeunesse" />
                             <div class="overlay">
@@ -114,7 +128,7 @@
                             </div>
                         </a>
                     </div>
-                    <div class="col-6">
+                    <div class="col-4">
                         <a href="/LectureDuJour/pages/comic.php" class="image fit">
                             <img src="/LectureDuJour/images/BD.jpg" alt="BD" />
                             <div class="overlay">
@@ -124,31 +138,10 @@
                     </div>
                 </div>
             </section>
-        -->
-        <!--
-		</div>
+        </div>
 
-		<hr/>
-		-->
-        <div class="row">
+        <div class="row" style="margin-top: 0;">
             <div class="col-12">
-
-                <!-- Contact -->
-                <section class="contact">
-                    <header>
-                        <h3>Retrouvez-moi ici</h3>
-                    </header>
-                    <ul class="icons">
-                        <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-                        <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-                        <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-                        <li><a href="#" class="icon fa-pinterest"><span class="label">Pinterest</span></a></li>
-                        <li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
-                        <li><a href="#" class="icon fa-linkedin"><span class="label">Linkedin</span></a></li>
-                    </ul>
-                </section>
-
-                <!-- Copyright -->
                 <div class="copyright">
                     <ul class="menu">
                         <li>&copy; LectureDuJour. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
@@ -156,7 +149,6 @@
                 </div>
 
             </div>
-
         </div>
     </div>
 </div>
